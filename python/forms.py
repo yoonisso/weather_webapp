93b0baa3cb42f41c5f2001b136663d95e5608ba4
moosipatrick <patrick.moosmann03@gmail.com>
2024-01-31 15:18:08 +0100

    integerrangefield

--- python/forms.py
+++ python/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, SelectField, DateField, FloatField, DecimalRangeField, DecimalField
+from wtforms import StringField, SubmitField, IntegerField, SelectField, DateField, FloatField, DecimalRangeField, DecimalField, IntegerRangeField
 from wtforms.validators import DataRequired, NumberRange
 
 class MyFloatField(FloatField):
@@ -14,7 +14,7 @@ class MyFloatField(FloatField):
 class searchForm(FlaskForm):
     longitude = DecimalField('Längengrad', places=6) 
     latitude = DecimalField('Breitengrad', places=6)
-    radius =  DecimalRangeField('Radius', validators = [NumberRange(5, 100)])
+    radius =  IntegerRangeField('Radius', validators = [NumberRange(5, 100)])
     startYear = IntegerField('Startjahr', validators = [NumberRange(1800, 2999)])
     endYear = IntegerField('Endjahr', validators = [NumberRange(1800, 2999)])
     stationCount = IntegerField('Anzahl Stationen', validators= [NumberRange(1, 20)])
