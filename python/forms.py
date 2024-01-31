0642e09b87c9e3ced257b0bd5b22ab05003663e6
moosipatrick <patrick.moosmann03@gmail.com>
2024-01-31 14:23:12 +0100

    form update

--- python/forms.py
+++ python/forms.py
@@ -1,11 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, SelectField, DateField, FloatField, DecimalRangeField
+from wtforms import StringField, SubmitField, IntegerField, SelectField, DateField, FloatField, DecimalRangeField, DecimalField
 from wtforms.validators import DataRequired, NumberRange
 
+class MyFloatField(FloatField):
+    def process_formdata(self, valuelist):
+        if valuelist:
+            try:
+                self.data = float(valuelist[0].replace(',', '.'))
+            except ValueError:
+                self.data = None
+                raise ValueError(self.gettext('Not a valid float value'))
+
 class searchForm(FlaskForm):
-    longitude = FloatField('Längengrad') 
-    latitude = FloatField('Breitengrad')
-    valueRange =  DecimalRangeField('Radius', validators = [NumberRange(5, 100)])
+    longitude = DecimalField('Längengrad', places=6) 
+    latitude = DecimalField('Breitengrad', places=6)
+    radius =  DecimalRangeField('Radius', validators = [NumberRange(5, 100)])
     startYear = IntegerField('Startjahr', validators = [NumberRange(1800, 2999)])
     endYear = IntegerField('Endjahr', validators = [NumberRange(1800, 2999)])
     stationCount = IntegerField('Anzahl Stationen', validators= [NumberRange(1, 20)])
+
--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, flash, redirect, url_for
 from forms import searchForm
 import secrets
+import sys
 
 foo = secrets.token_urlsafe(16)
 
@@ -10,10 +11,10 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 app.secret_key = foo
 
 class searchData:
-    def __init__(self,longitude,latitude,valueRange,startYear,endYear,stationCount):
+    def __init__(self,longitude,latitude,radius,startYear,endYear,stationCount):
         self.longitude = longitude
         self.latitude = latitude
-        self.valueRange = valueRange
+        self.radius = radius
         self.startYear = startYear
         self.endYear = endYear
         self.stationCount = stationCount
@@ -23,10 +24,21 @@ class searchData:
 def home():
     form = searchForm(request.form)
 
+    #Standort Fürth
+    form.latitude.data = 49.4771
+    form.longitude.data = 10.9887
+
+    #Standard-Werte
+    form.radius.data = 50
+    form.stationCount.data = 5
+    form.startYear.data = 2000 #Keine Anforderung
+    form.endYear.data = 2024 #Keine Anforderung
+    
     if request.method == 'POST' and form.validate_on_submit():
-        s1 = searchData(form.longitude,form.latitude,form.valueRange,form.startYear, form.endYear,form.stationCount)
-        flash(s1.longitude)
+        s1 = searchData(form.longitude.data,form.latitude.data,form.radius.data,form.startYear.data, form.endYear.data,form.stationCount.data)
+        
         return redirect(url_for('home'))
+    
     return render_template('Startseite.html', form=form)
 
 @app.route("/jahresansicht")
