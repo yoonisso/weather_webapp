59cd97ba5b46a796d795ee2a34d4d0b2888abf52
moosipatrick <patrick.moosmann03@gmail.com>
2024-01-31 09:53:50 +0100

    html + css

--- python/main.py
+++ python/main.py
@@ -4,8 +4,23 @@ app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('Startseite.html')
 
+@app.route("/jahresansicht")
+def jahresansicht():
+    return render_template('Jahresansicht.html')
+
+@app.route("/liste")
+def liste():
+    return render_template('Liste.html')
+
+@app.route("/monatsansicht")
+def monatsansicht():
+    return render_template('Monatsansicht.html')
+
+@app.route("/tagesansicht")
+def tagesansicht():
+    return render_template('Tagesansicht.html')
 
 
 if __name__ == '__main__':
