8d10d0de4a5e8597c473fcd3dd9c91ac20540aa7
moosipatrick <patrick.moosmann03@gmail.com>
2024-02-02 16:17:12 +0100

    Testdaten durchschnittstemperatur

--- python/main.py
+++ python/main.py
@@ -70,30 +70,61 @@ def yearView(id):
     form = searchForm(request.form)
     
     if request.method == 'GET':
-        stationTemperatures = getWeatherDataOfStationByStationId(id, session['startYear'], session['endYear'])
-        stationTemperatures = dict(stationTemperatures)
-        if not stationTemperatures:
-            flash('Der ausgewählte Zeitraum enthält keine Daten')
-        else:
-            session['stationWeatherData'] = stationTemperatures
-            #Mittelwerte berechnen
+
+        #! Folgendes nicht löschen
+        
+        # stationTemperatures = getWeatherDataOfStationByStationId(id, session['startYear'], session['endYear'])
+        # stationTemperatures = dict(stationTemperatures)
+
+        # if not stationTemperatures:
+        #     flash('Der ausgewählte Zeitraum enthält keine Daten')
+        # else:
+        #     session['stationWeatherData'] = stationTemperatures
+        #     #Mittelwerte berechnen
                             
-            averageTemperaturesYear = defaultdict(lambda: dict())
-            #Test Mittelwert Jahr
-            for year in stationTemperatures:
-                divisor = 0
-                sumMin = 0
-                sumMax = 0
-                for month in stationTemperatures[year]:
-                    for day in stationTemperatures[year][month]:
-                        divisor += 1
-                        sumMin += stationTemperatures[year][month][day]['TMIN']
-                        sumMax += stationTemperatures[year][month][day]['TMAX']
-                averageTemperaturesYear[year]['TMIN'] = round(sumMin/divisor,1)
-                averageTemperaturesYear[year]['TMAX'] = round(sumMax/divisor,1)
+        #     averageTemperaturesYear = defaultdict(lambda: dict())
+        #     #Test Mittelwert Jahr
+        #     for year in stationTemperatures:
+        #         divisor = 0
+        #         sumMin = 0
+        #         sumMax = 0
+        #         for month in stationTemperatures[year]:
+        #             for day in stationTemperatures[year][month]:
+        #                 divisor += 1
+        #                 sumMin += stationTemperatures[year][month][day]['TMIN']
+        #                 sumMax += stationTemperatures[year][month][day]['TMAX']
+        #         averageTemperaturesYear[year]['TMIN'] = round(sumMin/divisor,1)
+        #         averageTemperaturesYear[year]['TMAX'] = round(sumMax/divisor,1)
+    
+    
+        #TESTDATEN
+        averageTemperaturesYear = {
+                        1949: {'TMIN': 4.3, 'TMAX': 14.7},
+                        1950: {'TMIN': 5.1, 'TMAX': 15.2},
+                        1951: {'TMIN': 3.8, 'TMAX': 14.5},
+                        1952: {'TMIN': 6.2, 'TMAX': 16.8},
+                        1953: {'TMIN': 4.5, 'TMAX': 15.0},
+                        1954: {'TMIN': 5.3, 'TMAX': 15.7},
+                        1955: {'TMIN': 4.8, 'TMAX': 14.9},
+                        1956: {'TMIN': 6.0, 'TMAX': 16.5},
+                        1957: {'TMIN': 5.7, 'TMAX': 15.4},
+                        1958: {'TMIN': 4.1, 'TMAX': 14.2},
+                        1959: {'TMIN': 5.6, 'TMAX': 15.8},
+                        1960: {'TMIN': 4.9, 'TMAX': 15.1},
+                        1961: {'TMIN': 5.2, 'TMAX': 15.3},
+                        1962: {'TMIN': 4.4, 'TMAX': 14.6},
+                        1963: {'TMIN': 5.0, 'TMAX': 15.6},
+                        1964: {'TMIN': 6.1, 'TMAX': 16.2},
+                        1965: {'TMIN': 4.7, 'TMAX': 15.5},
+                        1966: {'TMIN': 5.4, 'TMAX': 15.9},
+                        1967: {'TMIN': 4.2, 'TMAX': 14.3},
+                        1968: {'TMIN': 5.5, 'TMAX': 16.0},
+                        # Füge hier weitere Jahre mit den entsprechenden Temperaturwerten hinzu
+                    }
 
 
-    return render_template('Jahresansicht.html',form=form, averageTemperaturesYear = dict(averageTemperaturesYear), id=id)
+
+    return render_template('Jahresansicht.html',form=form, averageTemperaturesYear = averageTemperaturesYear, id=id)
 
 @app.route("/liste", methods=['POST', 'GET'])
 def list():
