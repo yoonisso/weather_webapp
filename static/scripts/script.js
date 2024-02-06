2359cac368aa101af329528e22d4b57f3a8dbb24
WegFlexx <niclas.aberle02@gmail.com>
2024-02-06 16:46:40 +0100

    column at list added

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,3 +1,12 @@
+const resizeObserver = new ResizeObserver(entries => {
+	entries.forEach(entry => {
+		document.querySelector (".image_container").innerHTML = `width: ${entry.contentRect.width} height: ${entry.contentRect.height}`;
+	});
+});
+
+const elems = document.querySelector('.image_container');
+myObserver.observe(elems);
+
 function outputUpdate(radius) {
     document.querySelector('#selected_radius').value = radius + " km";
 }
