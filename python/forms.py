6596f02c6adc05f2de40e45491350c7167160868
moosipatrick <patrick.moosmann03@gmail.com>
2024-02-06 21:35:34 +0100

    form update

--- python/forms.py
+++ python/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, IntegerField, SelectField, DateField, FloatField, DecimalRangeField, DecimalField, IntegerRangeField
-from wtforms.validators import DataRequired, NumberRange,InputRequired
+from wtforms.validators import DataRequired, NumberRange,InputRequired, ValidationError
 
 class MyFloatField(FloatField):
-    def process_formdata(self, valuelist):
+    def process_formdata(self, valuelist): #TODO: Wird überhaupt verwendet?
         if valuelist:
             try:
                 self.data = float(valuelist[0].replace(',', '.'))
@@ -12,10 +12,18 @@ class MyFloatField(FloatField):
                 raise ValueError(self.gettext('Not a valid float value'))
 
 class searchForm(FlaskForm):
-    latitude = DecimalField('Breitengrad', places=6, validators=[InputRequired()])
-    longitude = DecimalField('Längengrad', places=6, validators=[InputRequired()]) 
+
+    def validate_end_year(form, field):
+        if(form.start_year.data > form.end_year.data):
+            
+            raise ValidationError('Endjahr muss größer oder gleich Startjahr sein')
+
+    latitude = DecimalField('Breitengrad', places=6, validators=[InputRequired(message="Bitte Breitengrad angeben")])
+    longitude = DecimalField('Längengrad', places=6, validators=[InputRequired(message="Bitte Breitengrad angeben")]) 
     radius =  IntegerRangeField('Radius', validators = [NumberRange(1, 100),InputRequired()] )
     start_year = IntegerField('Startjahr', validators = [NumberRange(1800, 2999), InputRequired()])
-    end_year = IntegerField('Endjahr', validators = [NumberRange(1800, 2999), InputRequired()])
+    end_year = IntegerField('Endjahr', validators = [NumberRange(1800, 2999), InputRequired(), validate_end_year])
     station_count = IntegerField('Anzahl Stationen', validators= [NumberRange(1, 20), InputRequired()])
+
+  
 
