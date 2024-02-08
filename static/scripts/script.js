861a37f62765deec0ee1b4929faf6ebac2971717
WegFlexx <niclas.aberle02@gmail.com>
2024-02-08 16:53:30 +0100

    rows clickable with js

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,4 +1,12 @@
-
 function outputUpdate(radius) {
     document.querySelector('#selected_radius').value = radius + " km";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const rows = document.querySelectorAll("tr[data-href]");
+    rows.forEach(row => {
+        row.addEventListener("click", () => {
+            window.location.href = row.dataset.href;
+        });
+    });
+});
