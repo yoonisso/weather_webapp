6c49a511eb11d4126e31092aba89493ebf7f84e4
Yoonis S. <yoonissoppke@gmail.com>
2024-02-13 11:28:39 +0100

    C-14: added Legend for Year,Month and Day view

--- python/helpers/diagram_ploter.py
+++ python/helpers/diagram_ploter.py
@@ -1,6 +1,6 @@
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.models import HoverTool, CustomJS
+from bokeh.models import HoverTool, Legend
 
 #Testdata yearly
 averageTemperaturesYear = {
@@ -44,9 +44,6 @@ averageTemperaturesMonthly = {
 }
 
 class DiagramPloter:
-    #TODO create Legend
-    #TODO fix hovereffect, add time of year to hover
-    #TODO wert ändern bei hover effekt
     @staticmethod
     def plotYearDiagram(averageTemperatures, chosen_views):
         min_width = 300
@@ -85,169 +82,180 @@ class DiagramPloter:
 
         p.add_tools(hover)
 
+        legend_items = []
+
         #year TMAX plot
         if chosen_views['year']['TMAX']:
-            p.line(
+            line_tmax = p.line(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['year']['TMAX'] for data in averageTemperatures.values()],
                 color=year_tmax_color,
                 line_width=3
             )
 
-            p.circle(
+            circle_tmax = p.circle(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['year']['TMAX'] for data in averageTemperatures.values()],
                 color=year_tmax_color,
                 size=10
             )
+            legend_items.append(("TMAX Jahr", [line_tmax, circle_tmax]))
 
         #year TMIN plot
         if chosen_views['year']['TMIN']:
-            p.line(
+            line_tmin = p.line(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['year']['TMIN'] for data in averageTemperatures.values()],
                 color=year_tmin_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_tmin = p.circle(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['year']['TMIN'] for data in averageTemperatures.values()],
                 color=year_tmin_color,
                 size=10
             )
-
-        #Spring TMIN
-        if chosen_views['spring']['TMIN']:
-            p.line(
-                x = [year for year in averageTemperatures.keys()],
-                y = [data['spring']['TMIN'] for data in averageTemperatures.values()],
-                color=spring_tmin_color,
-                line_width=4
-            )
-
-            p.circle(
-                x = [year for year in averageTemperatures.keys()],
-                y = [data['spring']['TMIN'] for data in averageTemperatures.values()],
-                color=spring_tmin_color,
-                size=10
-            )
-
+            legend_items.append(("TMIN Jahr", [line_tmin, circle_tmin]))
 
         #Spring TMAX
         if chosen_views['spring']['TMAX']:
-            p.line(
+            line_spring_tmax = p.line(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['spring']['TMAX'] for data in averageTemperatures.values()],
                 color=spring_tmax_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_spring_tmax = p.circle(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['spring']['TMAX'] for data in averageTemperatures.values()],
                 color=spring_tmax_color,
                 size=10
             )
+            legend_items.append(("TMAX Frühling", [line_spring_tmax, circle_spring_tmax]))
 
-        #summer TMIN
-        if chosen_views['summer']['TMIN']:
-            p.line(
+        #Spring TMIN
+        if chosen_views['spring']['TMIN']:
+            line_spring_tmin = p.line(
                 x = [year for year in averageTemperatures.keys()],
-                y = [data['summer']['TMIN'] for data in averageTemperatures.values()],
-                color=summer_tmin_color,
+                y = [data['spring']['TMIN'] for data in averageTemperatures.values()],
+                color=spring_tmin_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_spring_tmin = p.circle(
                 x = [year for year in averageTemperatures.keys()],
-                y = [data['summer']['TMIN'] for data in averageTemperatures.values()],
-                color=summer_tmin_color,
+                y = [data['spring']['TMIN'] for data in averageTemperatures.values()],
+                color=spring_tmin_color,
                 size=10
             )
-
+            legend_items.append(("TMIN Frühling", [line_spring_tmin, circle_spring_tmin]))
 
         #Summer TMAX
         if chosen_views['summer']['TMAX']:
-            p.line(
+            line_summer_tmax = p.line(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['summer']['TMAX'] for data in averageTemperatures.values()],
                 color=summer_tmax_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_summer_tmax = p.circle(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['summer']['TMAX'] for data in averageTemperatures.values()],
                 color=summer_tmax_color,
                 size=10
             )
+            legend_items.append(("TMAX Sommer", [line_summer_tmax, circle_summer_tmax]))
 
-        #Fall TMIN
-        if chosen_views['fall']['TMIN']:
-            p.line(
+        #summer TMIN
+        if chosen_views['summer']['TMIN']:
+            line_summer_tmin = p.line(
                 x = [year for year in averageTemperatures.keys()],
-                y = [data['fall']['TMIN'] for data in averageTemperatures.values()],
-                color=fall_tmin_color,
+                y = [data['summer']['TMIN'] for data in averageTemperatures.values()],
+                color=summer_tmin_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_summer_tmin = p.circle(
                 x = [year for year in averageTemperatures.keys()],
-                y = [data['fall']['TMIN'] for data in averageTemperatures.values()],
-                color=fall_tmin_color,
+                y = [data['summer']['TMIN'] for data in averageTemperatures.values()],
+                color=summer_tmin_color,
                 size=10
             )
-
+            legend_items.append(("TMIN Sommer", [line_summer_tmin, circle_summer_tmin]))
 
         #Fall TMAX
         if chosen_views['fall']['TMAX']:
-            p.line(
+            line_fall_tmax = p.line(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['fall']['TMAX'] for data in averageTemperatures.values()],
                 color=fall_tmax_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_fall_tmax = p.circle(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['fall']['TMAX'] for data in averageTemperatures.values()],
                 color=fall_tmax_color,
                 size=10
             )
+            legend_items.append(("TMAX Herbst", [line_fall_tmax, circle_fall_tmax]))
 
-        #Winter TMIN
-        if chosen_views['winter']['TMIN']:
-            p.line(
+        #Fall TMIN
+        if chosen_views['fall']['TMIN']:
+            line_fall_tmin = p.line(
                 x = [year for year in averageTemperatures.keys()],
-                y = [data['winter']['TMIN'] for data in averageTemperatures.values()],
-                color=winter_tmin_color,
+                y = [data['fall']['TMIN'] for data in averageTemperatures.values()],
+                color=fall_tmin_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_fall_tmin = p.circle(
                 x = [year for year in averageTemperatures.keys()],
-                y = [data['winter']['TMIN'] for data in averageTemperatures.values()],
-                color=winter_tmin_color,
+                y = [data['fall']['TMIN'] for data in averageTemperatures.values()],
+                color=fall_tmin_color,
                 size=10
             )
-
+            legend_items.append(("TMIN Herbst", [line_fall_tmin, circle_fall_tmin]))
 
         #Winter TMAX
         if chosen_views['winter']['TMAX']:
-            p.line(
+            line_winter_tmax=p.line(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['winter']['TMAX'] for data in averageTemperatures.values()],
                 color=winter_tmax_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_winter_tmax=p.circle(
                 x = [year for year in averageTemperatures.keys()],
                 y = [data['winter']['TMAX'] for data in averageTemperatures.values()],
                 color=winter_tmax_color,
                 size=10
             )
+            legend_items.append(("TMAX Winter", [line_winter_tmax, circle_winter_tmax]))
+
+        #Winter TMIN
+        if chosen_views['winter']['TMIN']:
+            line_winter_tmin = p.line(
+                x = [year for year in averageTemperatures.keys()],
+                y = [data['winter']['TMIN'] for data in averageTemperatures.values()],
+                color=winter_tmin_color,
+                line_width=4
+            )
+
+            circle_winter_tmin= p.circle(
+                x = [year for year in averageTemperatures.keys()],
+                y = [data['winter']['TMIN'] for data in averageTemperatures.values()],
+                color=winter_tmin_color,
+                size=10
+            )
+            legend_items.append(("TMIN Winter", [line_winter_tmin, circle_winter_tmin]))
+
+        legend = Legend(items=legend_items)
+        p.add_layout(legend, 'right')
 
         script, div = components(p)
 
@@ -275,41 +283,49 @@ class DiagramPloter:
                    x_axis_label="Monat", y_axis_label='Temperatur',
                    toolbar_location=None, tools=[])
 
-        hover = HoverTool(tooltips=TOOLTIPS)
+        hover = HoverTool(tooltips=[("Monat", "@x"), ("Temperatur", "@y{0.0}°C")])
 
         p.add_tools(hover)
 
+        legend_items = []
+
         #month TMAX plot
         if show_tmax:
-            p.line(
+            line_tmax = p.line(
                 x = [month for month in averageTemperatures.keys()],
                 y = [data['TMAX'] for data in averageTemperatures.values()],
                 color=month_tmax_color,
                 line_width=3
             )
 
-            p.circle(
+            circle_tmax = p.circle(
                 x = [month for month in averageTemperatures.keys()],
                 y = [data['TMAX'] for data in averageTemperatures.values()],
                 color=month_tmax_color,
                 size=10
             )
+            legend_items.append(("TMAX Monat", [line_tmax, circle_tmax]))
 
         #month TMIN plot
         if show_tmin:
-            p.line(
+            line_tmin = p.line(
                 x = [month for month in averageTemperatures.keys()],
                 y = [data['TMIN'] for data in averageTemperatures.values()],
                 color=month_tmin_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_tmin = p.circle(
                 x = [month for month in averageTemperatures.keys()],
                 y = [data['TMIN'] for data in averageTemperatures.values()],
                 color=month_tmin_color,
                 size=10
             )
+            legend_items.append(("TMIN Monat", [line_tmin, circle_tmin]))
+
+        legend = Legend(items=legend_items)
+        p.add_layout(legend, 'right')
+
         script, div = components(p)
 
         return script, div
@@ -336,41 +352,49 @@ class DiagramPloter:
                    x_axis_label='Tag', y_axis_label='Temperatur',
                    toolbar_location=None, tools=[])
 
-        hover = HoverTool(tooltips=TOOLTIPS)
+        hover = HoverTool(tooltips=[("Tag", "@x"), ("Temperatur", "@y{0.0}°C")])
 
         p.add_tools(hover)
 
+        legend_items = []
+
         #day TMAX plot
         if show_tmax:
-            p.line(
+            line_tmax = p.line(
                 x = [day for day in averageTemperatures.keys()],
                 y = [data['TMAX'] for data in averageTemperatures.values()],
                 color=day_tmax_color,
                 line_width=3
             )
 
-            p.circle(
+            circle_tmax = p.circle(
                 x = [day for day in averageTemperatures.keys()],
                 y = [data['TMAX'] for data in averageTemperatures.values()],
                 color=day_tmax_color,
                 size=10
             )
+            legend_items.append(("TMAX Tag", [line_tmax, circle_tmax]))
 
         #day TMIN plot
         if show_tmin:
-            p.line(
+            line_tmin = p.line(
                 x = [day for day in averageTemperatures.keys()],
                 y = [data['TMIN'] for data in averageTemperatures.values()],
                 color=day_tmin_color,
                 line_width=4
             )
 
-            p.circle(
+            circle_tmin = p.circle(
                 x = [day for day in averageTemperatures.keys()],
                 y = [data['TMIN'] for data in averageTemperatures.values()],
                 color=day_tmin_color,
                 size=10
             )
+            legend_items.append(("TMIN Tag", [line_tmin, circle_tmin]))
+
+        legend = Legend(items=legend_items)
+        p.add_layout(legend, 'right')
+
         script, div = components(p)
 
         return script, div
